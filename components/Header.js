acbdb63335b19d56f32e0d6797a01239c30bea01
Abdu-Ak <abduak1425378@gmail.com>
2023-12-17 16:57:52 +0530

    completed.

--- components/Header.js
+++ components/Header.js
@@ -7,11 +7,12 @@ const Header = () => {
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-          <Link href={'/'}>
+          <Link href={'/'}  className="pt-[85px]">
             <Image
-              src={"/logo.svg"}
-              width={220}
-              height={48}
+              src={"/text.png"}
+          
+              width={440}
+              height={96}
               alt="logo"
               priority={true}
             />
